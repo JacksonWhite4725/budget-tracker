358bb37816dcf21ea9513f4452edd2ff4a7cde65
JacksonWhite4725 <jawh4725@colorado.edu>
2021-11-09 08:14:16 -0700

    Added webpack with proper routes

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+  entry: './public/index.js',
+  output: {
+    path: __dirname + '/public/dist',
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+    plugins: [
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        inject: false,
+        fingerprints: false,
+        name: 'Budget Tracker app',
+        short_name: 'Budget',
+        description: 'An application that allows you to track a budget, whether you\'re online or offline',
+        background_color: '#01579b',
+        theme_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: path.resolve(__dirname, 'public/icons/icon-512x512.png'),
+            sizes: [192, 512],
+          },
+        ],
+      }),
+    ],
+  };
+
+module.exports = config;
