cc976c2d9a69354b19f88821c3876fae787c8c92
JacksonWhite4725 <jawh4725@colorado.edu>
2021-11-09 11:11:10 -0700

    Fixed service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,6 +5,7 @@ const FILES_TO_CACHE = [
     '/index.js',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
     '/manifest.json',
+    './dist/bundle.js',
     './icons/icon-192x192.png',
     './icons/icon-512x512.png'
 ];
@@ -12,26 +13,26 @@ const FILES_TO_CACHE = [
 const STATIC_CACHE = 'static-cache-v1';
 const RUNTIME_CACHE = 'runtime-cache';
   
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches
-            .open(STATIC_CACHE)
+        caches.open(STATIC_CACHE)
             .then(cache => cache.addAll(FILES_TO_CACHE))
-            .then(self.skipWaiting())
+            .then(() => self.skipWaiting())
     );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
     const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
     event.waitUntil(
-        caches
-            .keys()
-            .then((cacheNames) => {
-                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+        caches.keys()
+            .then(cacheNames => {
+                return cacheNames.filter(
+                    cacheName => !currentCaches.includes(cacheName)
+                );
             })
-            .then((cachesToDelete) => {
+            .then(cachesToDelete => {
                 return Promise.all(
-                    cachesToDelete.map((cacheToDelete) => {
+                    cachesToDelete.map(cacheToDelete => {
                         return caches.delete(cacheToDelete);
                     })
                 );
@@ -40,7 +41,7 @@ self.addEventListener('activate', (event) => {
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
     if (event.request.method !== 'GET' || !event.request.url.startsWith(self.location.origin)) {
         event.respondWith(fetch(event.request));
         return;
@@ -48,16 +49,12 @@ self.addEventListener('fetch', (event) => {
 
     if (event.request.url.includes('/api/transaction')) {
         event.respondWith(
-            caches
-                .open(RUNTIME_CACHE)
-                .then(cache => {
-                    return fetch(event.request)
-                    .then(response => {
-                        cache.put(event.request, response.clone());
-                        return response;
-                    })
-                    .catch(() => caches.match(event.request));
-                })
+            caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(event.request).then(response => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                }).catch(() => caches.match(event.request));
+            })
         );
         return;
     }
@@ -68,13 +65,9 @@ self.addEventListener('fetch', (event) => {
                 return cachedResponse;
             }
 
-            return caches
-                .open(RUNTIME_CACHE)
-                .then(cache => {
-                    return fetch(event.request)
-                    .then(response => {
-                        return cache.put(event.request, response.clone())
-                        .then(() => {
+            return caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(event.request).then(response => {
+                    return cache.put(event.request, response.clone()).then(() => {
                             return response;
                         });
                     });
