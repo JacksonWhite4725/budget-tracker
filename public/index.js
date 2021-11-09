523df7e27d0180e9ab3b6d7120eb21339562bcfb
JacksonWhite4725 <jawh4725@colorado.edu>
2021-11-09 11:57:07 -0700

    Added MongoDB functionallity to index.js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,63 @@
 let transactions = [];
 let myChart;
+let db;
+let budgetVersion;
+
+const request = indexedDB.open('budgetdb', budgetVersion || 35);
+
+request.onupgradeneeded = function(e) {
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
+  db = e.target.result;
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore('BudgetStore', { autoincrement: true });
+  }
+};
+
+request.onerror = function(e) {
+  console.log(`${e.target.errorCode}`);
+};
+
+function checkDatabase() {
+  let transaction = db.transaction(['BudgetStore'], 'readwrite');
+  const store = transaction.objectStore('BudgetStore');
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((response) => response.json())
+      .then((res) => {
+        if (res.length !== 0) {
+          transaction = db.transaction(['BudgetStore'], 'readwrite');
+          const currentStore = transaction.objectStore('BudgetStore');
+          currentStore.clear();
+        }
+      });
+    }
+  };
+};
+
+request.onsuccess = function(e) {
+  db = e.target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  };
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+  const store = transaction.objectStore('BudgetStore');
+  store.add(record);
+};
+
 
 fetch("/api/transaction")
   .then(response => {
@@ -151,3 +209,5 @@ document.querySelector("#add-btn").onclick = function() {
 document.querySelector("#sub-btn").onclick = function() {
   sendTransaction(false);
 };
+
+window.addEventListener('online', checkDatabase)
